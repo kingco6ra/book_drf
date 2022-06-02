cd25efba9aec8274b69d07230638745c03346ec0
cobra <king.co6ra@yandex.ru>
2022-06-02 12:41:04 +0300

    optimize

--- store/tests/test_serializers.py
+++ store/tests/test_serializers.py
@@ -7,30 +7,30 @@ from store.serializers import BookSerializer
 
 class BookSerializerTestCase(TestCase):
     def test_serializer_data(self):
-        book_1 = Book.objects.create(title='Test_1', author='Author 1', price='199.99')
-        book_2 = Book.objects.create(title='Test_2', author='Author 2', price='99.99')
         user = User.objects.create(username='test_user')
+
+        book_1 = Book.objects.create(title='Test_1', author='Author 1', price=15)
+        book_2 = Book.objects.create(title='Test_2', author='Author 2', price=25)
 
         data = BookSerializer([book_1, book_2], many=True).data
         expected_data = [
             {
                 'id': book_1.id,
                 'title': 'Test_1',
+                'price': '15.00',
                 'author': 'Author 1',
-                'price': '199.99',
+                'likes': 0,
             },
             {
                 'id': book_2.id,
                 'title': 'Test_2',
+                'price': '25.00',
                 'author': 'Author 2',
-                'price': '99.99',
+                'likes': 0,
             }
         ]
 
-        print(*data)
-        print('----------')
-        print(*expected_data)
-        self.assertEqual(data, expected_data)
+        self.assertEqual(expected_data, data)
 
     def test_serializer_str(self):
         title = 'Test Book'
